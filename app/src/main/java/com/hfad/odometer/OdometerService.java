aadf659617209c747213cf42e58903ca6eb3eb34
etxeran <funbo@hotmail.com>
2017-03-21 07:36:33 +0100

    Initial commit

---
+++ app/src/main/java/com/hfad/odometer/OdometerService.java
@@ -0,0 +1,25 @@
+package com.hfad.odometer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+(529)
+public class OdometerService extends Service {
+    private final IBinder binder = new OdometerBinder();
+    public OdometerService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    public class OdometerBinder extends Binder {
+        OdometerService getOdometer() {
+            return OdometerService.this;
+        }
+    }
+}
