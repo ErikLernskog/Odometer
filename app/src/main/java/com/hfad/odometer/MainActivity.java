0a5ee7f4860b1bd61a73b30c82132d6b34dd0f65
ErikLernskog <funbo@hotmail.com>
2017-03-27 07:33:15 +0200

    Done

--- app/src/main/java/com/hfad/odometer/MainActivity.java
+++ app/src/main/java/com/hfad/odometer/MainActivity.java
@@ -6,24 +6,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
     private OdometerService odometer;
     private boolean bound = false;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Intent intent = new Intent(this, OdometerService.class);
-        bindService(intent, connection, Context.BIND_AUTO_CREATE);
-    }
 
     private ServiceConnection connection = new ServiceConnection() {
         @Override
@@ -37,4 +27,44 @@ public class MainActivity extends Activity {
             bound = false;
         }
     };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        watchMileage();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, OdometerService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (bound) {
+            unbindService(connection);
+            bound = false;
+        }
+    }
+
+    private void watchMileage() {
+        final TextView distanceView = (TextView) findViewById(R.id.distance);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                double distance = 0.0;
+                if (odometer != null) {
+                    distance = odometer.getMiles();
+                }
+                String distanceStr = String.format("%1$,.2f miles", distance);
+                distanceView.setText(distanceStr);
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
 }
